888d9f75874e417931f953f9d2284b3da91ff156
Aasif Ahmad <84038203+aasif-ahmad12307@users.noreply.github.com>
2025-07-13 13:40:22 +0530

    Add files via upload

---
+++ greenroots/src/main/java/com/dbuu/greenroots/GreenrootsApplication.java
@@ -0,0 +1,13 @@
+package com.dbuu.greenroots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenrootsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenrootsApplication.class, args);
+	}
+
+}
